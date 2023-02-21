54eeccbf0538d8401feed1b6fabef5a878a2ee07
nencyahir <125643703+nencyahir@users.noreply.github.com>
2023-02-21 16:47:04 +0530

    Update assign_1.rb

--- ROR/Date_20_2/assign_1.rb
+++ ROR/Date_20_2/assign_1.rb
@@ -1,18 +1,18 @@
 require 'date'
 
 
-birthday = DateTime.new(2002,4,1,2,30,0)
+birthdate = DateTime.new(2002,4,1,2,30,0)
 
 
 puts "Current time is:" + Time.now.to_s
 puts"Birthday is: #{ birthday.to_time}"
 
-age_in_seconds = Time.now - birthday.to_time
-age_in_minutes = age_in_seconds / 60
-age_in_hours = age_in_minutes / 60
-age_in_days = age_in_hours / 24
-age_in_months = age_in_days / 30.44
-age_in_years = age_in_months / 12
+ageseconds = Time.now - birthday.to_time
+ageminutes = ageseconds / 60
+agenhours = ageminutes / 60
+agedays = agehours / 24
+agemonths = agedays / 30.44
+ageyears = agemonths / 12
 
 
-puts "Age is #{age_in_years.to_i} year #{(age_in_months % 12).to_i} month #{(age_in_days % 30.44).to_i} days #{(age_in_hours % 24).to_i} hours and #{(age_in_minutes % 60).to_i} minutes"
+puts "Age is #{ageyears.to_i} year #{(agemonths % 12).to_i} month #{(agedays % 30.44).to_i} days #{(agehours % 24).to_i} hours and #{(ageminutes % 60).to_i} minutes"
